12a98d33b3c9a24d8a18bca893875a0f5712e676
Tony Suarez <suarezcumare@gmail.com>
2015-10-30 05:50:09 +0000

    commit inicial

---
+++ config/initializers/assets.rb
@@ -0,0 +1,30 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( simple-line-icons.css )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( custom.js )
+Rails.application.config.assets.precompile += %w( imagesloaded.pkgd.js )
+Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
+Rails.application.config.assets.precompile += %w( jquery.cbpQTRotator.js )
+Rails.application.config.assets.precompile += %w( jquery.easypiechart.js )
+Rails.application.config.assets.precompile += %w( jquery.sticky.js )
+Rails.application.config.assets.precompile += %w( jquery.parallax-1.1.3.js )
+Rails.application.config.assets.precompile += %w( jquery.sticky.js )
+Rails.application.config.assets.precompile += %w( modernizr.custom.js )
+Rails.application.config.assets.precompile += %w( smoothscroll.js )
+Rails.application.config.assets.precompile += %w( waypoints.min.js )
+Rails.application.config.assets.precompile += %w( wow.min.js )
+
